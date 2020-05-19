4b02030eb12efbadf1263872a538129271cb9640
Philip Jordan <p.j.nz@outlook.com>
2020-05-20 09:22:18 +1200

    GeometryService license added

--- src/main/java/org/jogamp/java3d/GeometryService.java
+++ src/main/java/org/jogamp/java3d/GeometryService.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2020 JogAmp Community. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation. The JogAmp Community designates 
+ * this particular file as subject to the "Classpath" exception as provided
+ * by Sun in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * The views and conclusions contained in the software and documentation are those
+ * of the authors and should not be interpreted as representing official policies,
+ * either expressed or implied, of the JogAmp Community.
+ *
+ */
 
 package org.jogamp.java3d;
 
